f3a5a37b72c35cb7c7892818f8be856cf0b5d3f9
ndedlamini80-lgtm <ndedlamini80@gmail.com>
2026-02-12 22:29:34 +0200

    Add files via upload

---
+++ replace.py
@@ -0,0 +1,12 @@
+#assigning " The!quick!brown!fox!jumps!over!the!lazy!dog." to the variable sentence
+sentence = "The!quick!brown!fox!jumps!over!the!lazy!dog."
+
+print(sentence.replace("!" ," ")) # prints the quick brown fox jumps over the lazy dog.
+
+new_sentence ="The quick brown fox jumps over the lazy dog."
+print(new_sentence.upper())  #prints THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG.
+
+
+sentence2 = "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG."
+position = sentence2[::-1]
+print(position) #prints .GOD YZAL EHT REVO SPMUJ XOF NWORB KCIUQ EHT
---
+++ strings.py.py
@@ -0,0 +1,5 @@
+#assigning "$$$Superman$$$" to the variable hero
+hero  = "$$$Superman$$$"
+
+print(hero.strip("$"))
+#prints out superman
